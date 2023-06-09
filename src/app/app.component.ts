678f003b7743789081c3de0e16f9b952e6af1ee7
arthur <a.lannelucq@gmail.com>
2023-06-09 10:35:15 +0200

    feat: display another image when user change drink type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Drink } from "./drink.enum";
 
+const EMPTY_WATER_URL = 'https://cdn-icons-png.flaticon.com/512/3100/3100553.png';
+const FILLED_WATER_URL = 'https://cdn-icons-png.flaticon.com/512/3100/3100566.png';
+const EMPTY_COFFEE_URL = 'https://cdn-icons-png.flaticon.com/512/924/924412.png';
+const FILLED_COFFEE_URL = 'https://cdn-icons-png.flaticon.com/512/924/924514.png';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +15,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  Drink = Drink;
   indexes = [0, 1, 2, 3, 4];
-  emptyImageUrl = 'https://cdn-icons-png.flaticon.com/512/3100/3100553.png';
-  filledImageUrl = 'https://cdn-icons-png.flaticon.com/512/3100/3100566.png';
   quantity = signal(2);
+  type = signal(Drink.Water);
+  emptyImageUrl = computed(() => this.type() === Drink.Water ? EMPTY_WATER_URL : EMPTY_COFFEE_URL);
+  filledImageUrl = computed(() => this.type() === Drink.Water ? FILLED_WATER_URL : FILLED_COFFEE_URL);
 
   decrement() {
     this.quantity.update(quantity => quantity ? quantity - 1 : 0);
